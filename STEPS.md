1ff56b75c6c0926eaf9bb18edf69a2fdd10b690c
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-09 12:11:31 +0000

    Final Code

---
+++ STEPS.md
@@ -0,0 +1,7 @@
+# Installation
+
+1. pnpm create vite@latest .
+2. pnpm install
+3. pnpm run dev
+
+# Folders & files
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./global.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const appEl = document.querySelector("#app");
+const root = createRoot(appEl);
+
+root.render(
+  <div className="min-h-screen flex justify-center items-center bg-blue-100">
+    <Watch></Watch>
+  </div>
+);
+
+function Watch() {
+  return (
+    <h1 className="text-6xl text-red-500 bg-blue-200 p-3 w-fit rounded-lg">
+      ⌚ This is a watch.
+    </h1>
+  );
+}
