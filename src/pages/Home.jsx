d1ce8d5f62cf7aca47e77a7558a827aa9576911e
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-10 11:27:55 +0000

    Add alias @ for src

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Header from "@/components/Header";
+import Form from "@/components/Form";
+import Aside from "@/components/Aside";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <Form />
+      <Aside />
+    </div>
+  );
 }
 
 export default Home;
