a02208698dfdbaf5cf63679cedf5d7f0385a5640
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-10 11:57:28 +0000

    working on layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,17 @@ import Aside from "@/components/Aside";
 
 function Home() {
   return (
-    <div>
+    <div className="min-h-screen bg-red-100 px-4 py-3 md:px-8">
       <Header />
-      <Form />
-      <Aside />
+
+      <div className="grid grid-cols-8">
+        <div className="col-span-2 hidden bg-green-100 md:block">
+          <Form />
+        </div>
+        <div className="col-span-full bg-blue-100 md:col-span-6">
+          <Aside />
+        </div>
+      </div>
     </div>
   );
 }
