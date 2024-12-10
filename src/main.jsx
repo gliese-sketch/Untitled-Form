3fa77fe5c85cb2ed37e6cbe117a75e7b1db6e227
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-10 11:17:11 +0000

    Create home component

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Home from "./pages/Home";
 import "./global.css";
 
 const appEl = document.querySelector("#app");
 const root = createRoot(appEl);
-
 root.render(
-  <div className="min-h-screen flex justify-center items-center bg-blue-100">
-    <Watch></Watch>
-  </div>
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
 );
-
-function Watch() {
-  return (
-    <h1 className="text-6xl text-red-500 bg-blue-200 p-3 w-fit rounded-lg">
-      ⌚ This is a watch.
-    </h1>
-  );
-}
